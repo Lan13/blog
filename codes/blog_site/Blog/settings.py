d8a41c3d9236f9209943dd99ffb11d2e2c6805d2
ljw13 <ljw13@mail.ustc.edu.cn>
2022-02-09 16:15:56 +0800

    hide secret key

--- codes/blog_site/Blog/settings.py
+++ codes/blog_site/Blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hykarp4+mogami*gq3#bai4fn%=a9mq$9#$t*55f6jf%i*d%9q'
+SECRET_KEY = 'THIS KEY IS HIDDEN'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
